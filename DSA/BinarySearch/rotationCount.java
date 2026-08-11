cd220be28a59e984fa9a62c23a43f4302ff15bd5
Deva Singh <deva81002@gmail.com>
2026-08-12 00:27:21 +0530

    Rotation count in rotated sorted array

--- DSA/BinarySearch/rotationCount.java
+++ DSA/BinarySearch/rotationCount.java
@@ -1,18 +1,17 @@
 
 //https://www.geeksforgeeks.org/dsa/find-rotation-count-rotated-sorted-array/public class rotationCount {
-    public static void main(String[] args) {
-        int[] nums = { 15, 18, 2, 3, 6, 12 };
-        int start = 0;
-        int end = nums.length - 1;
-        while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] < nums[end]) {
-                end = mid;
-            } else {
-                start = mid + 1;
-            }
+public static void main(String[] args) {
+    int[] nums = { 15, 18, 2, 3, 6, 12 };
+    int start = 0;
+    int end = nums.length - 1;
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] < nums[end]) {
+            end = mid;
+        } else {
+            start = mid + 1;
         }
-        System.out.println(start);
-
     }
+    System.out.println(start);
 
+}
