19b63023c4373feef33a52506f41ce976f9cbb65
Deva Singh <deva81002@gmail.com>
2026-08-16 16:45:10 +0530

    square root of a number without using Math.sqrt method

---
+++ DSA/maths_for_dsa/squareRoot.java
@@ -0,0 +1,34 @@
+// package DSA.maths_for_dsa;
+
+public class squareRoot {
+    public static void main(String[] args) {
+        int num = 259;
+        int p = 3;
+        System.out.println(sqrt(num, p));
+
+    }
+
+    static double sqrt(int num, int p) {
+        int start = 0;
+        int end = num;
+        double root = 0.0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if ((long) mid * mid <= num) {
+                root = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        double incr = 0.1;
+        for (int i = 0; i < p; i++) {
+            while (root * root <= num) {
+                root += incr;
+            }
+            root -= incr;
+            incr /= 10;
+        }
+        return root;
+    }
+}
