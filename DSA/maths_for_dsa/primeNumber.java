adb25a816120dc1f10ab2ff1eeb1d0fac9bca1e5
Deva Singh <deva81002@gmail.com>
2026-08-17 23:21:57 +0530

    correction in code

--- DSA/maths_for_dsa/primeNumber.java
+++ DSA/maths_for_dsa/primeNumber.java
@@ -2,18 +2,19 @@
 
 public class primeNumber {
     public static void main(String[] args) {
-        int num = 40;
+        // int num = 1;
+        // int num = 40;
+        int num = 13;
         boolean ans = isPrime(num);
         System.out.println(ans);
     }
 
     static boolean isPrime(int num) {
         int c = 2;
+        if (num <= 1) {
+            return false;
+        }
         while (c * c <= num) {
-            if (num <= 1) {
-                return false;
-            }
-
             if (num % c == 0) {
                 return false;
             }
