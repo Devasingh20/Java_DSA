47266dd6a32db06aff8b2930939a82d0f7747cf8
Deva Singh <deva81002@gmail.com>
2026-08-24 21:35:48 +0530

    Binary search using recursion

---
+++ DSA/Recursion/binarySearch.java
@@ -0,0 +1,21 @@
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 55, 66, 78 };
+        int target = 79;
+        System.out.println(BS(arr, target, 0, arr.length - 1));
+    }
+
+    static int BS(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int mid = s + (e - s) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        if (target < arr[mid]) {
+            return BS(arr, target, s, mid - 1);
+        }
+        return BS(arr, target, mid + 1, e);
+    }
+}
