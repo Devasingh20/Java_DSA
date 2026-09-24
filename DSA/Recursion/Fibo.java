3200c4659cbaa665b02d840b22d1e411a038e1e2
Deva Singh <deva81002@gmail.com>
2026-09-24 19:56:37 +0530

    when you use fibo(n - 1) + fibo(n - 2) you will failed to find n fibo number when n is large so, by using ((1/Math.sqrt(5))*(Math.pow((1 + Math.sqrt(5)) / 2,n))) you will easily found nth fibo number

--- DSA/Recursion/Fibo.java
+++ DSA/Recursion/Fibo.java
@@ -1,31 +1,32 @@
-// public class Fibo {
-//     public static void main(String[] args) {
-//         System.out.println(fibo(6));
-//     }
-
-//     static int fibo(int n) {
-//         if (n < 2) {
-//             return n;
-//         }
-//         return fibo(n - 1) + fibo(n - 2);
-//     }
-// }
-
-//series
 public class Fibo {
     public static void main(String[] args) {
-        fibo(6);
+        System.out.println(fibo(50));
     }
 
-    static void fibo(int n) {
-        int a = 0;
-        int b = 1;
-        System.out.print(a + " " + b + " ");
-        for (int i = 2; i < n; i++) {
-            int c = a + b;
-            System.out.print(c + " ");
-            a = b;
-            b = c;
+    static long fibo(long n) {
+        if (n < 2) {
+            return n;
         }
+       //return fibo(n - 1) + fibo(n - 2);
+       return (long) ((1/Math.sqrt(5))*(Math.pow((1 + Math.sqrt(5)) / 2,n)));
     }
 }
+    
+//series
+// public class Fibo {
+//     public static void main(String[] args) {
+//         fibo(6);
+//     }
+
+//     static void fibo(int n) {
+//         int a = 0;
+//         int b = 1;
+//         System.out.print(a + " " + b + " ");
+//         for (int i = 2; i < n; i++) {
+//             int c = a + b;
+//             System.out.print(c + " ");
+//             a = b;
+//             b = c;
+//         }
+//     }
+// }
