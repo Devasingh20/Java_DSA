2a0a5be6eb58a845e1882476b61e79e8d95299f0
Deva Singh <deva81002@gmail.com>
2026-09-25 09:59:02 +0530

    reverse of a number using recursion

---
+++ DSA/Recursion/Reverse.java
@@ -0,0 +1,19 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int n = 123456789;
+        reverse(n);
+        System.out.println(sum);
+    }
+
+    static int sum = 0;
+
+    static void reverse(int n) {
+        if (n == 0) {
+            return;
+        }
+        int digit = n % 10;
+        sum = sum * 10 + digit;
+        reverse(n / 10);
+    }
+
+}
