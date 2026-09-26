a35a46c16cf43c2e6ba50b19251c0381f4bd99b5
Deva Singh <deva81002@gmail.com>
2026-09-26 22:50:22 +0530

    Basic list

---
+++ DSA/list/BasicList.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class BasicList {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        System.out.println("List: " + list);
+    }
+}
